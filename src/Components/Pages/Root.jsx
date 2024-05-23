7663d7314ed77a860f3a989dfbbd31febbc5330a
Sumaiya Urmi <akthersumaiya689@gmail.com>
2024-05-24 02:26:06 +0600

    Footer added

--- src/Components/Pages/Root.jsx
+++ src/Components/Pages/Root.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Navbar from './Shared/Navbar';
+import Footer from './Shared/Footer';
 
 const Root = () => {
     return (
         <div className='font-openSens'>
             <Navbar></Navbar>
+
+            <Footer></Footer>
         </div>
     );
 };
